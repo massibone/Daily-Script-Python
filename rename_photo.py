7a67cae4efbd68de96bcb0e5efa80d3de6ddd1df
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-09-26 10:51:22 +0200

    Create rename_photo.py

---
+++ rename_photo.py
@@ -0,0 +1,6 @@
+## Batch Image Renamer (Fuji X100V)
+#Script per rinominare in automatico file .jpg generati da Fuji.  
+### Uso  
+#python renamer.py --path dir  
+### Output  
+#Rinominazione batch con data + localizzazione.
