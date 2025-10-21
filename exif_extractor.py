9fae8b88c1840be779b1835547d4ddafbe120330
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-10-21 08:29:49 +0200

    Update exif_extractor.py

--- exif_extractor.py
+++ exif_extractor.py
@@ -1,3 +1,12 @@
 """
 Estrae le informazioni EXIF dalle immagini e le salva in formato CSV
 """
+
+import os
+import csv
+import sys
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+import argparse
+from datetime import datetime
+import logging
