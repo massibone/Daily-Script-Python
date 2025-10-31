d66fe1302cef44a1321c951733f4e412cc92a963
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-10-31 10:09:55 +0100

    Update exif_extractor.py

--- exif_extractor.py
+++ exif_extractor.py
@@ -14,6 +14,7 @@ import logging
 # Configurazione logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
 class ExifExtractor:
     def __init__(self):
         self.supported_formats = ('.jpg', '.jpeg', '.tiff', '.tif')
