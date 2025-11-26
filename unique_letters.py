b1f59a81fe43c2092481e2106236cdf11eb7aa12
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-11-26 08:32:52 +0100

    Create unique_letters.py

---
+++ unique_letters.py
@@ -0,0 +1,6 @@
+# As a simple application of sets, let's use
+# sets to count the number of unique letters in a word.
+
+sentence = 'Jimquicklyrealized'
+count_letters = set(sentence)
+len(count_letters)
