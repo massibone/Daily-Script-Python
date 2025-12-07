591b4089c96c0737725c3aea8662e16602628742
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-12-07 17:03:47 +0100

    Update genera_caption.py

--- social_tools/genera_caption.py
+++ social_tools/genera_caption.py
@@ -1,3 +1,8 @@
 '''
 Usa LLM per creare caption engaging da una descrizione scatto, con emoji e CTA per massimizzare interazioni su foto X100V 
 '''
+
+import openai
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
