59c3452ca42153dd605b1080a765acd1c7dd004a
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-12-10 08:24:40 +0100

    Update exif_organizer.py

--- image_processing/exif_organizer.py
+++ image_processing/exif_organizer.py
@@ -2,3 +2,9 @@
 Organizzatore foto basato su EXIF:
 crea cartelle in formato YYYY-MM/ISO e sposta i file.
 """
+from pathlib import Path
+from typing import List
+import argparse
+from PIL import Image
+from PIL.ExifTags import TAGS
+import shutil
