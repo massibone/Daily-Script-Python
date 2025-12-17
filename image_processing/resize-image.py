def5863f18207cba2136ebbe7b57afca8730a333
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-12-17 08:33:46 +0100

    Update resize-image.py

--- image_processing/resize-image.py
+++ image_processing/resize-image.py
@@ -1,1 +1,12 @@
 
+from PIL import Image
+import os
+
+def resize_folder(input_folder, output_folder, size=(640, 480)):
+    for filename in os.listdir(input_folder):
+        if filename.endswith('.jpg'):
+            img = Image.open(os.path.join(input_folder, filename))
+            img = img.resize(size)
+  
+img.save(os.path.join(output_folder, filename))
+
