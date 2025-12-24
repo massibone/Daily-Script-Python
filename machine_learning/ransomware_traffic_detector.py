01cebe2bf4d7c8ad0167809d4a2230e7d3676310
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-12-24 09:13:44 +0100

    Update ransomware_traffic_detector.py

--- machine_learning/ransomware_traffic_detector.py
+++ machine_learning/ransomware_traffic_detector.py
@@ -1,1 +1,13 @@
+import scapy.all as scapy
+import pandas as pd
+from sklearn.ensemble import IsolationForest
+
+def packet_features(pkt):
+    # Estrai features semplici dal pacchetto (esempio)
+    return {
+        'src': pkt[0][1].src,
+        'dst': pkt[0][1].dst,
+        'len': len(pkt),
+        'proto': pkt[0][1].proto
+    }
 
