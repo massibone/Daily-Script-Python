4a4aeb7690a5eab89216894eda4ebd4e8d3f8d87
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2026-01-06 09:05:37 +0100

    Delete upload_images.py

--- upload_images.py
+++
@@ -1,7 +0,0 @@
-import os
-from github import Github
-
-g = Github('TOKEN')
-repo = g.get_repo('username/project')
-for img in os.listdir('imgs'):
-    repo.create_file(f'images/{img}', f'Add {img}', open(f'imgs/{img}', 'rb').read())
