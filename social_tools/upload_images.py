e89608551346bef424bc8d91815072200777ae9e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2026-01-06 09:05:13 +0100

    Update upload_images.py

--- social_tools/upload_images.py
+++ social_tools/upload_images.py
@@ -1,1 +1,7 @@
+import os
+from github import Github
 
+g = Github('TOKEN')
+repo = g.get_repo('username/project')
+for img in os.listdir('imgs'):
+    repo.create_file(f'images/{img}', f'Add {img}', open(f'imgs/{img}', 'rb').read())
