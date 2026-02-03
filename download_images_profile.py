a6df8d19104f8c56edfc76b76a53277d849e9de5
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2026-02-03 09:43:25 +0100

    Delete download_images_profile.py

--- download_images_profile.py
+++
@@ -1,6 +0,0 @@
-# Estrazione immagini da Instagram
-import instaloader
-def download_images(profile):
-    loader = instaloader.Instaloader()
-    for post in instaloader.Profile.from_username(loader.context, profile).get_posts():
-        loader.download_post(post, target=profile)
