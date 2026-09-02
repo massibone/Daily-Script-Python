a83e37d640e59239836062ba255ccb15ab487922
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2026-09-02 09:30:18 +0200

    Add docstring to anonimizza_feedback function
    Added a docstring for the anonimizza_feedback function.

--- data_privacy/anonimizza_feedback.py
+++ data_privacy/anonimizza_feedback.py
@@ -2,6 +2,7 @@ import re
 import pandas as pd
 from io import StringIO
 
+
 def anonimizza_feedback(feedback_data):
     """
     Anonimizza una lista di feedback sostituendo dati personali con token pseudonimi.
